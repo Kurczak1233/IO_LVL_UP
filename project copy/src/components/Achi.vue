ef91cdda6b7acccfd1a83683acc4c579ff780077
Michał Kupczak <kurczak12333@gmail.com>
2021-01-25 19:23:45 +0100

    Add content to Achi

--- project copy/src/components/Achi.vue
+++ project copy/src/components/Achi.vue
@@ -1,12 +1,12 @@
 <template>
-<b-container>
+<b-container class="background-color" fluid>
     <b-row class="fluid">
     <b-col cols="12">
         <img id="banner" class="img-fluid text-center pt-5 pb-5" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Kurczak1233/project%20copy/src/assets/PlanetImg.png?raw=true" alt="Logo projektu"> 
     </b-col>
-        <b-col class="text-center d-none d-md-block" cols="4">Test</b-col>
-        <b-col class="text-center d-none d-md-block" cols="4">Test</b-col>
-        <b-col class="text-center d-none d-md-block" cols="4">Test</b-col>
+        <b-col class="text-center d-none d-md-block" cols="4">Mamy ponad 30000 'zarejestrowanych użytkowników (pod)'</b-col>
+        <b-col class="text-center d-none d-md-block" cols="4">Posiadamy największa bazę materiałów do nauki!</b-col>
+        <b-col class="text-center d-none d-md-block" cols="4">Uzyskaliśmy 4.9/5.0 gwiazdek z opinii użytkowników</b-col>
     </b-row>
 </b-container>
 </template>
@@ -19,5 +19,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.background-color
+{
+    background-color: rgb(217,218,211);
+}
 </style>
