08e152260269cb925c0673461da87b20a2ed40b8
Michał Kupczak <kurczak12333@gmail.com>
2021-01-25 19:05:30 +0100

    Add new img png

--- project copy/src/components/Achi.vue
+++ project copy/src/components/Achi.vue
@@ -1,7 +1,7 @@
 <template>
 <b-container>
     <b-row class="fluid">
-        <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Kurczak1233/project%20copy/src/assets/Glob.jpg?raw=true" alt="Logo projektu"> 
+        <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Kurczak1233/project%20copy/src/assets/59-592818_globus-png-globus-jpg-transparent-png.png?raw=true" alt="Logo projektu"> 
         <b-col class="text-center d-none d-md-block" cols="4">Test</b-col>
         <b-col class="text-center d-none d-md-block" cols="4">Test</b-col>
         <b-col class="text-center d-none d-md-block" cols="4">Test</b-col>
