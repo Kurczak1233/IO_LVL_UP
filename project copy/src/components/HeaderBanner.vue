a2d8639a13fb9a938edf827f4d0818900a85ed9b
Joanna Setla <asia.setla@gmail.com>
2021-01-27 17:16:45 +0100

    komentarz

--- project copy/src/components/HeaderBanner.vue
+++ project copy/src/components/HeaderBanner.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container text-center">
-        <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/Baner_JS.png?raw=true" alt="Logo projektu"> <!--Centrowanie zdjęcia (zdjęcie jest obiektem liniowym więc centrujemy text!!)-->
+        <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Baner_JS.png?raw=true" alt="Logo projektu"> <!--Centrowanie zdjęcia (zdjęcie jest obiektem liniowym więc centrujemy text!!)-->
     </div>
 </template>
 
