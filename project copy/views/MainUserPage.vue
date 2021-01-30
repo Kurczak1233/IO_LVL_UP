abd63461bb79f0d2230bfc1e502023fa8d453108
Michał Kupczak <kurczak12333@gmail.com>
2021-01-30 23:04:47 +0100

    Improving userpage

--- project copy/views/MainUserPage.vue
+++ project copy/views/MainUserPage.vue
@@ -6,9 +6,9 @@
           <b-col class="col-3 text-size-big ml-auto mr-auto text-success d-none d-xl-block"><router-link to="/UserPage"><img id="project-logo" class="img-fluid" src="https://github.com/Kurczak1233/Projekt_IO/blob/main/project/src/assets/Logo.png?raw=true" alt="Logo strony">IO_LVL_UP</router-link></b-col>
           <b-col class="col-4 text-success mb-auto mt-auto ml-auto mr-auto d-none d-xl-block">
             <b-list-group horizontal>
-  <b-list-group-item class="rounded-edges-left btn btn-success" variant="primary" href="#">Lessons</b-list-group-item>
-  <b-list-group-item variant="primary btn btn-warning" href="#">Quizes</b-list-group-item>
-  <b-list-group-item class="rounded-edges-right btn btn-danger" variant="primary" href="#">Progress</b-list-group-item>
+  <b-list-group-item class="rounded-edges-left btn btn-success" variant="success" href="#">Lessons</b-list-group-item>
+  <b-list-group-item variant="warning btn btn-warning" href="#">Quizes</b-list-group-item>
+  <b-list-group-item class="rounded-edges-right btn btn-danger" variant="danger" href="#">Progress</b-list-group-item>
             </b-list-group>
 </b-col>
           <b-col class="col-3 d-xl-none">  
@@ -25,8 +25,27 @@
       </b-container>
       </b-nav>
       <!---->
-        <b-container>
-          <b-col class="col-5" fluid id="aside"></b-col>
+        <b-container fluid>
+          <b-row>
+          <b-col class="col-4 pt-3 text-center color-bluish" fluid id="aside">
+              <b-row>
+                <b-col class="col-12 mb-3 text-center font-large">Your level: (player-level)</b-col>
+              </b-row>
+              <b-row>
+                <b-col class="col-6 text-center font-medium">Available classes:</b-col>
+                <b-list-group>
+  <b-list-group-item class="" variant="" href="#">Speaking</b-list-group-item>
+  <b-list-group-item class="" variant="" href="#">Writing</b-list-group-item>
+  <b-list-group-item class="" variant="" href="#">Grammar</b-list-group-item>
+  <b-list-group-item class="" variant="" href="#">Vocabulary</b-list-group-item>
+  <b-list-group-item class="" variant="" href="#">Listening</b-list-group-item>
+            </b-list-group>
+              </b-row>
+          </b-col>
+          <b-col class="col-8 text-center color-bluish" fluid id="main">
+            
+            tekst</b-col>
+          </b-row>
 
               <!-- <b-button v-b-toggle.sidebar-variant>Toggle Sidebar</b-button>  ZAJEBISTY SIDEBAR!
     <b-sidebar id="sidebar-variant" title="Sidebar" bg-variant="dark" text-variant="light" shadow>
@@ -66,5 +85,13 @@ export default {
 {
   border-bottom-right-radius: 20px;
   border-top-right-radius: 20px;
+}
+.font-large
+{
+  font-size: 1.5rem;
+}
+.font-medium
+{
+  font-size: 1.2rem;
 }
 </style>
