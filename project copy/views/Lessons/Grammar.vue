7a704d2d6b12c4ab68ddc48955d7edf3246d669e
S1Daniel <daniel.biegun46@onet.pl>
2021-01-31 15:22:37 +0100

    grammar_update2

--- project copy/views/Lessons/Grammar.vue
+++ project copy/views/Lessons/Grammar.vue
@@ -10,22 +10,78 @@
   <b-col class="">An important invention in the development of science and medicine was the microscope. It was (1) ... the principle that light could be (2) ... or bent, by a glass lens. It was soon discovered that tiny objects could be magnified (3) ... size when viewed through a glass lens that had been ground and polished in a specific (4)... . Although the principle was known to the Chinese as early as 1000 A.D, it was not until the 13th and 14th centuries in Europe (5) ... it was put to practical use in the form of eyeglasses.
   </b-col>
 </b-row>   
-
 <b-row class="">
   <b-col class="">In Europe the first microscope was invented by brothers Zacharias and Hans Janssen, two Dutch eyeglass-makers, around 1590. They built a “compound” microscope, so called because of its two lenses. The most significant development and use of the microscope during this period, however, belongs to another Dutch optician, Antonie van Leeuwenhoek (1632-1723). Born in Delft, Holland, he became skilled at (6) ... very sharp and accurate magnifying lenses. Some of his single-lens microscopes were able to
   </b-col>
 </b-row>   
-
 <b-row class="">
   <b-col class="">magnify up the three hundred times (7) ... size, and around 1660 he began serious study using these instruments. He was the first to discover bacteria and other microscopic organisms, calling these tiny creatures “animalcules”. (8) ... to this discovery, very small creatures such as fleas and maggots were thought to “spontaneously generate” from a single source, such as rotting meat in the case of maggots.
   </b-col>
 </b-row>   
 
-<b-row class="">
-  <b-col class="">
-  </b-col>
-</b-row>   
-
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="row">1</th>
+      <td>based upon</td>
+      <td>based at</td>
+      <td>based by</td>
+      <td>based in</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">2</th>
+      <td>retraced</td>
+      <td>retracted</td>
+      <td>refracted</td>
+      <td>retorted</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>on</td>
+      <td>in</td>
+      <td>by</td>
+      <td>with</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+       <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>current</td>
+      <td>actual</td>
+      <td>substantial</td>
+      <td>presentr</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Due</td>
+      <td>According</td>
+      <td>Owing</td>
+      <td>Prior</td>
+    </tr>
+      </tbody>
+</table>
+ 
 
     </b-container>  
 </b-container>
