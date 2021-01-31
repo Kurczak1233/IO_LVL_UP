db1c2304bd81476a8b0ce5e75e3e55336a997cc9
Michał Kupczak <kurczak12333@gmail.com>
2021-02-01 00:12:16 +0100

    Upgrade builder

--- project copy/views/Lessons/Writing.vue
+++ project copy/views/Lessons/Writing.vue
@@ -2,7 +2,7 @@
   <body>
     <b-container class="col-10 col-md-8 col-xl-5 mr-auto ml-auto margin10 background-bluish" fluid>
         <b-row>
-          <b-col class="col-12">Tekst: {{task}}</b-col>
+          <b-col class="col-12">Tekst: {{name}},{{description}},{{isDone}}</b-col>
         </b-row>
     </b-container>
   </body>
@@ -15,13 +15,57 @@
 
 // // przykład konstruktor Task oraz TaskBuilder
 
-// function Task(name, description, isDone){
+
+function Task(name, description, isDone){
+    this.name = name;
+    this.description = description;
+    this.isDone = isDone;
+}
+
+function TaskBuilder() {
+
+    return {
+        setName: function(name) {
+            this.name = name;
+            return this;
+        },
+        setDescription: function(description) {
+            this.description = description;
+            return this;
+        },
+        setDone: function(done) {
+            this.isDone = done;
+            return this;
+        },
+        build: function () {
+            return new Task(this.name, this.description, this.isDone);
+        }
+    }
+}
+// Dyrektor
+let task = (new TaskBuilder()).setName("Zadanie 1")
+        .setDescription("Opis zadania").setDone(true).build();
+// {name: "Zadanie 1", description: "opis", isDone: true}
+
+export default {
+    name: 'Grammar',
+        data: function()
+    {
+      return {
+        name: task.name,
+        description: task.description,
+        isDone: task.isDone
+      
+      }
+    }
+//     methods:
+//     {
+//     Task(name, description, isDone){
 //     this.name = name;
 //     this.description = description;
 //     this.isDone = isDone;
-// }
-
-// function TaskBuilder() {
+// },
+//       TaskBuilder() {
 
 //     return {
 //         setName: function(name) {
@@ -41,52 +85,8 @@
 //         }
 //     }
 // }
-// // Dyrektor
-// let task = (new TaskBuilder()).setName("Zadanie 1")
-//         .setDescription("opis").setDone(true).build();
-// // {name: "Zadanie 1", description: "opis", isDone: true}
-
-export default {
-    name: 'Grammar',
-        data: function()
-    {
-      return {task}
-    },
-    methods:
-    {
-    Task(name, description, isDone){
-    this.name = name;
-    this.description = description;
-    this.isDone = isDone;
-},
-      TaskBuilder() {
-
-    return {
-        setName: function(name) {
-            this.name = name;
-            return this;
-        },
-        setDescription: function(description) {
-            this.description = description;
-            return this;
-        },
-        setDone: function(done) {
-            this.isDone = done;
-            return this;
-        },
-        build: function () {
-            return new Task(this.name, this.description, this.isDone);
-        }
-    }
-},
-BuildTask()
-{
-let task = (new TaskBuilder()).setName("Zadanie 1")
-        .setDescription("opis").setDone(true).build();
-        return task;
+// }
 }
-}
-    }
 </script>
 
 <style >
