c795b63abffd369b770cd9b4135ab35dae45e9ad
S1Daniel <daniel.biegun46@onet.pl>
2021-01-31 15:03:56 +0100

    text_bootstrap

--- project copy/views/Lessons/Grammar.vue
+++ project copy/views/Lessons/Grammar.vue
@@ -1,40 +1,48 @@
 <template>
-  <body>
-    <b-container class="col-10 col-md-8 col-xl-5 mr-auto ml-auto margin10 background-bluish" fluid>
-            Tutaj wstawić quiz. Dynamiką sprawdzania "ONCLICK trzeba się też zająć..."
-    </b-container>
-  </body>
+<b-container class="container text-center">
+  <b-row class="">
+    <b-col class="mb-3" cols="12"> <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/src/assets/Baner_grammar.png?raw=true" alt="Logo grammar"></b-col>
+  </b-row>
+<b-container class="col-240 col-md-120 col-xl-100 mr-auto ml-auto margin5 background-bluish" fluid>
+            <b>THE MICROSCOPE</b>
+
+<b-row class="">
+  <b-col class="">An important invention in the development of science and medicine was the microscope. It was (1) ... the principle that light could be (2) ... or bent, by a glass lens. It was soon discovered that tiny objects could be magnified (3) ... size when viewed through a glass lens that had been ground and polished in a specific (4)... . Although the principle was known to the Chinese as early as 1000 A.D, it was not until the 13th and 14th centuries in Europe (5) ... it was put to practical use in the form of eyeglasses.
+  </b-col>
+</b-row>   
+
+<b-row class="">
+  <b-col class="">In Europe the first microscope was invented by brothers Zacharias and Hans Janssen, two Dutch eyeglass-makers, around 1590. They built a “compound” microscope, so called because of its two lenses. The most significant development and use of the microscope during this period, however, belongs to another Dutch optician, Antonie van Leeuwenhoek (1632-1723). Born in Delft, Holland, he became skilled at (6) ... very sharp and accurate magnifying lenses. Some of his single-lens microscopes were able to
+  </b-col>
+</b-row>   
+
+<b-row class="">
+  <b-col class="">magnify up the three hundred times (7) ... size, and around 1660 he began serious study using these instruments. He was the first to discover bacteria and other microscopic organisms, calling these tiny creatures “animalcules”. (8) ... to this discovery, very small creatures such as fleas and maggots were thought to “spontaneously generate” from a single source, such as rotting meat in the case of maggots.
+  </b-col>
+</b-row>   
+
+<b-row class="">
+  <b-col class="">
+  </b-col>
+</b-row>   
+
+
+    </b-container>  
+</b-container>
 </template>
-
-
-
 
 <script>
 export default {
-    name: 'Grammar'}
-</script>
 
-<style >
-body
-{
-  background-color: rgb(217,218,211);
-  width: 100%;
-  height:100%;
+
+
+name: 'HeaderBanner',
+  
 }
-.background-bluish
-{
-  background-color: rgb(105, 208, 245);
-  border-radius: 20px;
-}
-.margin10
-{
-  margin-top: 10%;
-}
-.text-size-big
-{
-  font-size: 2rem;
-}
-#project-logo{
-  height: 80px;
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#banner{
+  border-radius: 50px;
 }
 </style>
