7664b7f64afbef074bdd521979289394f97f4dc7
S1Daniel <daniel.biegun46@onet.pl>
2021-01-31 19:57:05 +0100

    listening quiz

--- project copy/views/Lessons/Listening.vue
+++ project copy/views/Lessons/Listening.vue
@@ -1,81 +1,54 @@
 <template>
 <b-container class="container text-center">
   <b-row class="">
-    <b-col class="mb-3" cols="12"> <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/src/assets/Baner_grammar.png?raw=true" alt="Logo grammar"></b-col>
+    <b-col class="mb-3" cols="12"> <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/src/assets/Baner_listening.png?raw=true" alt="Logo listening"></b-col>
   </b-row>
 <b-container class="col-240 col-md-120 col-xl-100 mr-auto ml-auto margin5 background-bluish" fluid>
-<b>THE MICROSCOPE</b>
-<b-row class="">
-  <b-col class=""><l>An important invention in the development of science and medicine was the microscope. It was (1) ... the principle that light could be (2) ... or bent, by a glass lens. It was soon discovered that tiny objects could be magnified (3) ... size when viewed through a glass lens that had been ground and polished in a specific (4)... . Although the principle was known to the Chinese as early as 1000 A.D, it was not until the 13th and 14th centuries in Europe (5) ... it was put to practical use in the form of eyeglasses.</l>
-  </b-col>
-</b-row>
-<b-row class="">
-  <b-col class=""><l>In Europe the first microscope was invented by brothers Zacharias and Hans Janssen, two Dutch eyeglass-makers, around 1590. They built a “compound” microscope, so called because of its two lenses. The most significant development and use of the microscope during this period, however, belongs to another Dutch optician, Antonie van Leeuwenhoek (1632-1723). Born in Delft, Holland, he became skilled at (6) ... very sharp and accurate magnifying lenses. Some of his single-lens microscopes were able to</l>
-  </b-col>
-</b-row>   
-<b-row class="">
-  <b-col class=""><l>magnify up the three hundred times (7) ... size, and around 1660 he began serious study using these instruments. He was the first to discover bacteria and other microscopic organisms, calling these tiny creatures “animalcules”. (8) ... to this discovery, very small creatures such as fleas and maggots were thought to “spontaneously generate” from a single source, such as rotting meat in the case of maggots.</l>
+<b>Kendall Jenner</b>
+
+<b-row class=""><audio src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/src/assets/listening.mp3?raw=true" controls ></audio>
+  <b-col class="">
   </b-col>
 </b-row>   
 <table class="table table-striped">
   <thead>
     <tr>
       <th scope="row">1</th>
-      <td><c>based upon</c></td>
-      <td><f>based at</f></td>
-      <td><f>based by</f></td>
-      <td><f>based in</f></td>
+      <td>Kendall spends a load of time in her house. </td>
+      <td><f>True</f></td>
+      <td><c>False</c></td>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">2</th>
-      <td><f>retraced</f></td>
-      <td><f>retracted</f></td>
-      <td><c>refracted</c></td>
-      <td><f>retorted</f></td>
+      <td>She used to walk dogs for a living.</td>
+      <td><c>True</c></td>
+      <td><f>False</f></td>
     </tr>
     <tr>
       <th scope="row">3</th>
-      <td><f>on</f></td>
-      <td><c>in</c></td>
-      <td><f>by</f></td>
-      <td><f>with</f></td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td><c>manner</c></td>
-      <td><f>fashion</f></td>
-      <td><f>character</f></td>
-      <td><f>procedure</f></td>
+      <td>Cancelled flights piss her off</td>
+      <td><f>True</f></td>
+      <td><c>False</c></td>
     </tr>
     <tr>
       <th scope="row">5</th>
-      <td><c>that</c></td>
-      <td><f>than</f></td>
-      <td><f>when</f></td>
-      <td><f>then</f></td>
+      <td>She spent a fortune on a vehicle.</td>
+      <td><c>True</c></td>
+      <td><f>False</f></td>
     </tr>
     <tr>
       <th scope="row">6</th>
-      <td><f>rubbing</f></td>
-      <td><f>granting</f></td>
-      <td><c>grinding</c></td>
-      <td><f>scraping</f></td>
+      <td>Chloe is her least favourite sister.</td>
+      <td><f>True</f></td>
+      <td><c>False</c></td>
     </tr>
     <tr>
       <th scope="row">7</th>
-      <td><f>current</f></td>
-      <td><c>actual</c></td>
-      <td><f>substantial</f></td>
-      <td><f>present</f></td>
-    </tr>
-    <tr>
-      <th scope="row">8</th>
-      <td><f>Due</f></td>
-      <td><f>According</f></td>
-      <td><f>Owing</f></td>
-      <td><c>Prior</c></td>
+      <td>She wishes she could become more independent.</td>
+      <td><f>True</f></td>
+      <td><c>False</c></td>
     </tr>
       </tbody>
 </table>
