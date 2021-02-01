bbf42a3fcce07c4909406b24372b91059caaf5bf
Michał Kupczak <kurczak12333@gmail.com>
2021-02-01 23:20:50 +0100

    Improve tests

--- project copy/tests/unit/Logging-button.spec.js
+++ project copy/tests/unit/Logging-button.spec.js
@@ -1,22 +1,23 @@
 import LoginButton from "@/components/LoggedInButton.vue";
 import {shallowMount} from "@vue/test-utils";
-
-const $router = {
-    replace: jest.fn()
-}
+import Footer from "@/components/Footer.vue";
 
 describe("LoggedInButton.vue", ()=> {
     let wrapper;
     beforeEach(()=> {
         wrapper = shallowMount(LoginButton, {
-          methods:   {setupFirebase: ()=> {}}
+           methods:   {setupFirebase: ()=> {}}
         })
     })
 
     it("renders", ()=> { //Test implemetnowania bazy danych (firebase)
         expect(wrapper.exists()).toBe(true);
     })
-    it("does button exists", ()=> { //Test szukania tekstu
-        expect(wrapper.find("button").text()).toBe("Log out")
+    it("does text log out exists", ()=> { //Test szukania tekstu
+        expect(wrapper.find("button").text()).toBe("Log out");
     })
+    it("does any div exists in button", ()=> {
+        expect(wrapper.find('div').exists()).toBe(true)
+    })
+    
 })
