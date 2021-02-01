b38e48dc5cef45a4474dfe280c4f67db0b52dda9
Michał Kupczak <kurczak12333@gmail.com>
2021-02-01 22:21:47 +0100

    Create simple unit test

---
+++ project copy/tests/unit/Logging-button.spec.js
@@ -0,0 +1,10 @@
+import LoginButton from "@/components/LoggedInButton.vue";
+import {shallowMount} from "@vue/test-utils";
+describe("LoggedInButton.vue", ()=> {
+    let wrapper;
+    beforeEach(()=> {
+        wrapper = shallowMount(TopHeader, {
+            
+        })
+    })
+})
