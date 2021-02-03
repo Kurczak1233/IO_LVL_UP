b68531c68b9300af9d56e8f257eef7eb5b686e7a
Michał Kupczak <kurczak12333@gmail.com>
2021-02-03 17:30:47 +0100

    Add new pictures

--- project copy/views/Advanced.vue
+++ project copy/views/Advanced.vue
@@ -1,11 +1,12 @@
 <template>
   <body>
-    <b-container class="col-10 col-md-8 col-xl-5 mr-auto ml-auto margin10 background-bluish" fluid>
+    <b-container class="col-11 col-md-11 col-xl-10 mr-auto ml-auto margin10 background-bluish" fluid>
         <b-row>
             <b-col class="col-12 text-center big-font">Advanced users</b-col>
         </b-row>
         <b-row>
-            <b-col class=""></b-col>
+            <b-col class="col-5"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Scale1.png?raw=true" class="height-700" alt=""></b-col>
+            <b-col class="col-7">Opis</b-col>
         </b-row>
     </b-container>
   </body>
