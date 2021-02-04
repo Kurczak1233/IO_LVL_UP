7db7748998d28da57860a84d8fb98e302018b4f5
Michał Kupczak <kurczak12333@gmail.com>
2021-02-04 23:31:08 +0100

    Implement

--- project copy/views/Offer.vue
+++ project copy/views/Offer.vue
@@ -12,10 +12,10 @@
         </b-row>
          <b-row class="pb-4 ml-4 height-300">
             <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto">There are mutliple sources of knowledge! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
-             <b-col class="col-5 d-block mt-auto mb-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Upgraded%20free%20logo.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
+             <b-col class="col-5 d-block mt-auto mb-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Books.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
         </b-row>
         <b-row class="pb-4 ml-4 height-300">
-            <b-col class="col-5 d-block mt-auto mb-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Upgraded%20free%20logo.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
+            <b-col class="col-5 d-block mt-auto mb-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Exams.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
             <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto">You are developing yourself with our instructions! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
         </b-row>
     </b-container>
