f5312334be2d35665bb0fa5d41e54cb69abde3fe
Michał Kupczak <kurczak12333@gmail.com>
2021-02-04 23:47:43 +0100

    Improve bootstrap

--- project copy/views/Offer.vue
+++ project copy/views/Offer.vue
@@ -6,17 +6,20 @@
             <b-col class="text-center col-12"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Asia/project%20copy/src/assets/logov2.png?raw=true" class="height-50 img-fluid img-fluid-class" alt="logo"></b-col>
         </b-row>
         <hr>
-        <b-row class="pb-4 ml-4 height-300">
-            <b-col class="col-5 d-block mt-auto mb-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Upgraded%20free%20logo.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
-            <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto">Our website is completly free! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
+        <b-row class="pb-4 ml-4 height-400">
+            <b-col class="col-12 d-xl-none text-center font-size-bg mt-5">Completely free materials!</b-col>
+            <b-col class="col-sm-8 col-md-5 col-xl-5 d-block mt-auto mb-auto mr-auto ml-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Upgraded%20free%20logo.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
+            <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto d-none d-xl-inline">Our website is completly free! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
         </b-row>
-         <b-row class="pb-4 ml-4 height-300">
-            <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto">There are mutliple sources of knowledge! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
-             <b-col class="col-5 d-block mt-auto mb-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Books.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
+         <b-row class="pb-4 ml-4 height-400">
+             <b-col class="col-12 d-xl-none text-center font-size-bg mt-5 mt-5">Unique knowledge resources!</b-col>
+            <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto d-none d-xl-inline">There are mutliple sources of knowledge! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
+             <b-col class="col-sm-8 col-md-5 col-xl-5 d-block mt-auto mb-auto mr-auto ml-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Books.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
         </b-row>
-        <b-row class="pb-4 ml-4 height-300">
-            <b-col class="col-5 d-block mt-auto mb-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Exams.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
-            <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto">You are developing yourself with our instructions! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
+        <b-row class="pb-4 ml-4 mb-5 height-500">
+            <b-col class="col-12 d-xl-none text-center font-size-bg mt-5">Guarantee success!</b-col>
+            <b-col class="col-sm-8 col-md-5 col-xl-5 d-block mt-auto mb-auto mr-auto ml-auto"><img src="https://github.com/Kurczak1233/IO_LVL_UP/blob/main/project%20copy/src/assets/Exams.png?raw=true" class="img-fluid img-fluid-class" alt="Cerf levels"></b-col>
+            <b-col class="col-7 text-indent font-size-md pr-5 mt-auto mb-auto d-none d-xl-inline">You are developing yourself with our instructions! ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae beatae dicta placeat voluptatum perferendis ipsum laboriosam eveniet saepe officia ullam ut culpa eius obcaecati, cupiditate assumenda quia labore explicabo. Facilis quibusdam consequuntur fugiat illum fuga ea saepe vel nostrum corporis laboriosam, suscipit voluptas ipsum, excepturi eveniet? Nisi illo accusantium quam laborum culpa suscipit incidunt omnis sint aperiam commodi? Rerum!</b-col>
         </b-row>
     </b-container>
   </body>
@@ -53,8 +56,16 @@ export default {
 {
     height: 200px;
 }
-.height-300
+.height-400
 {
     height:400px;
+}
+.height-500
+{
+    height:500px;
+}
+.font-size-bg
+{
+    font-size:30px;
 }
 </style>
