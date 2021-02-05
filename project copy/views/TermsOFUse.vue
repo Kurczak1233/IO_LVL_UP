5f1035b3d4f5c8339b9c5332f9632477c6f6a238
Karolina Baron <kb055869@student.ath.edu.pl>
2021-02-05 14:50:27 +0100

    REGULAMINS ;)

--- project copy/views/TermsOFUse.vue
+++ project copy/views/TermsOFUse.vue
@@ -1,18 +1,21 @@
 <template>
 <b-container class="container text-center">
   <b-row class="">
-    <b-col class="mb-2" cols="12"> <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Karolina/project%20copy/src/assets/favicon%20(2).png?raw=true%22" alt="Logo listening"></b-col>
+    <b-col class="mb-2" cols="12"> <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/src/assets/Baner_JS.png?raw=true" alt="Logo listening"></b-col>
   </b-row>
 <b-container class="col-12 mr-auto ml-auto margin5 background-bluish" fluid>
   <b-row>
-      <b-col class="text-center margin5"> Terms of use </b-col>
+      <b-col class="text-center margin5"> Terms of Use </b-col>
     </b-row>
 </b-container>
-<b-container class="col-12 mr-auto ml-auto margin10 background-bluish" fluid>
+   <b-col> <p> </p> </b-col>
+<b-container class="col-12 mr-auto ml-auto background-bluish" fluid>
 <table class="table table-striped">
   <thead>
     <tr>
-       <b-col class="text-center margin6"> The Website Name website located at Website.com is a copyrighted work belonging to Website Name. Certain features of the Site may be subject to additional guidelines, terms, or rules, which will be posted on the Site in connection with such features. All such additional terms, guidelines, and rules are incorporated by reference into these Terms. These Terms of Use described the legally binding terms and conditions that oversee your use of the Site. BY LOGGING INTO THE SITE, YOU ARE BEING COMPLIANT THAT THESE TERMS and you represent that you have the authority and capacity to enter into these Terms. YOU SHOULD BE AT LEAST 18 YEARS OF AGE TO ACCESS THE SITE. IF YOU DISAGREE WITH ALL OF THE PROVISION OF THESE TERMS, DO NOT LOG INTO AND/OR USE THE SITE. These terms require the use of arbitration Section 10.2 on an individual basis to resolve disputes and also limit the remedies available to you in the event of a dispute. </b-col>
+       <b-col class="text-left margin4"> <p> </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu nulla at neque rhoncus eleifend. Aenean ac arcu sem. Praesent mattis, neque eu molestie mollis, leo elit pellentesque lorem, eu sagittis lacus augue vitae orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vehicula malesuada ligula. Nam at laoreet massa, et consequat erat. Proin a sapien eget lorem scelerisque faucibus quis eleifend nulla. Pellentesque nec turpis sodales, sollicitudin turpis a, faucibus est. Maecenas accumsan convallis nisi a luctus. Vestibulum sollicitudin massa at arcu iaculis, eu pellentesque velit porttitor. Proin vel nisl porttitor lectus rhoncus scelerisque. Praesent blandit, quam sit amet condimentum luctus, nunc nulla laoreet odio, sit amet vehicula ex purus quis neque. Vivamus rhoncus leo elit, vel venenatis elit condimentum nec. Sed dapibus metus felis, a porta lorem iaculis sagittis. Nunc tempus ultrices purus id tristique. Nulla quis pulvinar lectus. Suspendisse elementum hendrerit ex, vitae ullamcorper tellus congue nec. Maecenas feugiat tempus mollis. </p> <p> Nulla dapibus lacinia malesuada. Nam non rutrum massa. Sed a finibus ante. Phasellus nec hendrerit metus, sit amet pulvinar diam. Donec tristique tincidunt leo, non venenatis turpis congue ut. Praesent ac mi vel arcu sollicitudin feugiat. Aenean vitae semper odio. Pellentesque id velit sit amet turpis commodo hendrerit vitae in urna. Nulla eget velit ac turpis blandit maximus a id nulla. Curabitur pellentesque neque et justo dignissim, dignissim tempor urna auctor. Donec pellentesque, justo eu cursus molestie, ligula lacus consequat metus, a maximus ligula diam vel ipsum. Integer sed diam vel mauris iaculis venenatis. Fusce eget porta nibh. Vivamus sit amet ex vitae justo volutpat faucibus. Integer volutpat sodales urna vitae dictum. Sed vitae dui libero. </p>
+        <p class="text-md-left text-center"><router-link to="/MainPage"> « « « Back to Main Page</router-link></p>
+ </b-col>
     </tr>
   </thead>
   </table>
