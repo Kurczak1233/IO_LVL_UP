5e4ec091232518e7d8be7b64594036aeff0cd7b2
Karolina Baron <kb055869@student.ath.edu.pl>
2021-02-05 23:02:23 +0100

    leadership

--- project copy/views/Leadership.vue
+++ project copy/views/Leadership.vue
@@ -15,6 +15,9 @@
     <tr>
        <b-col class="text-left margin4"> <p> </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu nulla at neque rhoncus eleifend. Aenean ac arcu sem. Praesent mattis, neque eu molestie mollis, leo elit pellentesque lorem, eu sagittis lacus augue vitae orci.  </p>
        <p> Nulla ullamcorper velit at ligula euismod, quis iaculis ipsum vestibulum. Ut convallis fringilla ex, id vehicula lectus facilisis ut. Phasellus efficitur malesuada nisl sit amet sodales. Fusce dignissim tempor elit, a mattis ex mattis nec. Pellentesque id malesuada nisl, id vulputate sapien. </p>
+         <div class="text-center"> <b-col class="mb-3" cols="12"> <img id="logo" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Karolina-v2/project%20copy/src/assets/lieadership.png?raw=true" alt="Logo listening"></b-col>  </div>
+        <p> Integer vel facilisis mauris. Duis et erat ac neque ultrices suscipit. Sed imperdiet ligula id dolor maximus molestie. Nulla lectus arcu, pulvinar at feugiat quis, tincidunt vel ex. Fusce non quam vel erat suscipit elementum in sed mauris. Mauris nec pellentesque ligula. Proin ultricies sapien lorem, vitae scelerisque orci porta id. Aenean eu egestas magna. Aliquam erat volutpat. Cras non dignissim diam. Aliquam id dui sit amet nunc faucibus congue a eu nunc. Maecenas turpis risus, porttitor lobortis neque id, pretium pulvinar lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce a enim mattis, accumsan odio id, gravida quam. </p>
+        <p> </p>
         <p class="text-md-left text-center"><router-link to="/MainPage"> « « « Back to Main Page</router-link></p>
  </b-col>
     </tr>
