351e657421dac4c645c36da45fd73c6f9a6fa964
Michał Kupczak <kurczak12333@gmail.com>
2021-02-12 18:55:12 +0100

    Implement view of placement test

--- project copy/views/PlacementTest.vue
+++ project copy/views/PlacementTest.vue
@@ -1,26 +1,31 @@
 <template>
   <body>
 
-    <b-container class="col-10 col-md-8 col-xl-5 mr-auto ml-auto background-bluish" fluid>
+    <b-container class="col-10 mt-5 col-md-8 col-xl-5 mr-auto ml-auto background-bluish" fluid>
         <b-row>
-          <b-col class="col-12"><b>Temat:</b> {{name}} <br> <b> Liczba punktów do uzyskania: </b> {{pkt}} <br> <b> Treść: </b> {{description}} <br> <b> Stopień trudności: </b> {{level}}<br><center>
-        <a href="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/src/assets/example/example1-1.jpg?raw=true">Przykładowe rozwiązanie</a>
-        </center></b-col>
+            <b-col class="text-center text-danger pt-2 text-size-big">
+                You are attempting a placement test.
+            </b-col>    
+        </b-row>
+        <b-row>
+          <b-col class="col-12 pt-3"><b> Available points: </b> 3</b-col>
+          <b-col class="col-12">Points will determine the level you are currently being on.</b-col>
+          <b-col class="pt-2 pb-3 text-center"><b>GOOD LUCK!</b></b-col>
         </b-row>
     </b-container>
-          <b-container class="col-10 mt-3 col-md-8 col-xl-5 mr-auto ml-auto background-bluish" fluid>
+    <b-container class="col-10 mt-3 col-md-8 col-xl-5 mr-auto ml-auto background-bluish" fluid>
         <b-row>
-          <b-col class="col-12 text-center">Proszę załączyć swoje rozwiązanie</b-col>
-          <b-col class="col-12 text-center"><form action="..." enctype="multipart/form-data">
-            <input type="file" name="nazwa">
-            </form>
-          </b-col>
+            <b-col class="col-12 text-center mt-3 mb-3">
+                    <button class="btn btn-warning">Start the test!</button>
+            </b-col>
         </b-row>
         </b-container>
   </body>
 </template>
 
 <script>
+//KONCEPCJA NA STWORZENIE QUIZU, liczenie pkt zadań i wskazywanie v-if/v-else? Jeśli 1 pkt pokaż tą podstronę itd?
+
 //Tworzenie zadania o imieniu, opisie i parametrze ukończenia.
 function Task(name, pkt, description, Islevel){
     this.name = name;
@@ -54,7 +59,7 @@ function TaskBuilder() {
     }
 }
 // Dyrektor rozkazujący utworzenie zadania
-let task = (new TaskBuilder()).setName("Mówienie: ") .setPkt("10")
+let task = (new TaskBuilder()).setName("Mówienie: ") .setPkt("3")
         .setDescription("Jakie są wady/zalety bycia singlem?").setLevel("B1").build();
 export default {
     name: 'Speaking',
