c275b14f4f64e10d7ef0d0321641472b6e67d35a
Michał Kupczak <kurczak12333@gmail.com>
2021-02-12 20:12:39 +0100

    Implement first test question

--- project copy/src/components/PlacementTestQuestion.vue
+++ project copy/src/components/PlacementTestQuestion.vue
@@ -3,15 +3,14 @@
     <b-container class="col-10 mt-5 col-md-8 col-xl-8 mr-auto ml-auto background-bluish" fluid>
         <b-row>
             <b-col class="text-center text-info pt-2 text-size-big">
-                Question {{name}}
+                Question {{id}}
             </b-col>    
         </b-row>
         <b-row>
-          <b-col class="col-12 pt-3 text-center">Today, Antarctica is mostly devoid of its ancient life. It has no trees or bushes and (1) ... is limited to moss and algae.</b-col>
-            <div>
-                <b-form-radio-group v-model="selected" :options="options" class="mb-3" value-field="item" text-field="name" disabled-field="notEnabled"></b-form-radio-group>
-                <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
-            </div>
+          <b-col class="col-10 offset-1 pt-3 text-center font-bigger">Today, Antarctica is mostly devoid of its ancient life. It has no trees or bushes and (1) ... is limited to moss and algae.</b-col>
+            <b-col class="d-flex align-items-center mt-3 mb-3" >
+                <b-form-radio-group v-model="selected" :options="options" class="text-center font-medium col-12" value-field="item" text-field="name" disabled-field="notEnabled"></b-form-radio-group>
+            </b-col>
         </b-row>
     </b-container>
   </body>
@@ -63,14 +62,13 @@ function TaskBuilder() {
             return this;
         },
         build: function () {
-            return new Task(this.id, this.pkt, this.description,this.answear1,this.answear2,this.answear3,this.answear4);
+            return new Task(this.id, this.pkt, this.description, this.answear1, this.answear2,this.answear3,this.answear4);
         }
     }
 }
 // Dyrektor rozkazujący utworzenie zadania
-let task = (new TaskBuilder()).setName("1") .setPkt("1")
-        .setDescription("Today, Antarctica is mostly devoid of its ancient life. It has no trees or bushes and (1) ... is limited to moss and algae.").setAnswear1("vegetables").setAnswear2("vegetations")
-        .setAnswear3("vegetation").setAnswear4("vegetating").build();
+let task = (new TaskBuilder()).setId("1").setPkt("1")
+        .setDescription("Today, Antarctica is mostly devoid of its ancient life. It has no trees or bushes and (1) ... is limited to moss and algae.").setAnswear1("vegetables").setAnswear2("vegetations").setAnswear3("vegetation").setAnswear4("vegetating").build();
 export default {
     name: 'Speaking',
         data: function()
@@ -79,54 +77,60 @@ export default {
         id: task.id,
         pkt: task.pkt,
         description: task.description,
+        answear1: task.answear1,
+        answear2: task.answear2,
+        answear3: task.answear3,
+        answear4: task.answear4,
         selected: 'A',
         options: [
-          { item: 'A', name: 'Option A' },
-          { item: 'B', name: 'Option B' },
-          { item: 'D', name: 'Option C', notEnabled: true },
-          { item: { d: 1 }, name: 'Option D' }
+          { item: "A", name: task.answear1 },
+          { item: "B", name: task.answear2 },
+          { item: 'C', value: "true" , name: task.answear3 },
+          { item: { d: 1 }, name: task.answear4 }
         ]
+      
       }
     },
     methods:
     {
-        CreateNewQuestion: function()
-        {
-            function Task(name, pkt, description){
-    this.name = name;
-    this.pkt = pkt;
-    this.description = description;
-}
-//Builder taska (pojedyncze funkcje nadają wartości typom prostym)
-function TaskBuilder() {
+//         CreateNewQuestion: function()
+//         {
+//             function Task(name, pkt, description){
+//     this.name = name;
+//     this.pkt = pkt;
+//     this.description = description;
+// }
+// //Builder taska (pojedyncze funkcje nadają wartości typom prostym)
+// function TaskBuilder() {
 
-    return {
-        setName: function(name) {
-            this.name = name;
-            return this;
-        },
-        setPkt: function(pkt) {
-            this.pkt = pkt;
-            return this;
-        },
-        setDescription: function(description) {
-            this.description = description;
-            return this;
-        },
-        build: function () {
-            return new Task(this.name, this.pkt, this.description);
-        }
+//     return {
+//         setName: function(name) {
+//             this.name = name;
+//             return this;
+//         },
+//         setPkt: function(pkt) {
+//             this.pkt = pkt;
+//             return this;
+//         },
+//         setDescription: function(description) {
+//             this.description = description;
+//             return this;
+//         },
+//         build: function () {
+//             return new Task(this.name, this.pkt, this.description);
+//         }
+//     }
+// }
+//         // Dyrektor rozkazujący utworzenie zadania
+//        return (new TaskBuilder()).setName("1") .setPkt("3")
+//         .setDescription("Jakie są wady/zalety bycia singlem?").setLevel("B1").build();
+//         },
+//         StartTest: function()
+//         {
+//             return this.questionNumber++;
+//         }
+//     },
     }
-}
-        // Dyrektor rozkazujący utworzenie zadania
-       return (new TaskBuilder()).setName("1") .setPkt("3")
-        .setDescription("Jakie są wady/zalety bycia singlem?").setLevel("B1").build();
-        },
-        StartTest: function()
-        {
-            return this.questionNumber++;
-        }
-    },
 }
 </script>
 
@@ -142,18 +146,11 @@ body
   background-color: rgb(105, 208, 245);
   border-radius: 20px;
 }
-.margin10
+.font-bigger
 {
-  margin-top: 10%;
+    font-size: 1.4rem;
 }
-.text-size-big
-{
-  font-size: 2rem;
-}
-#project-logo{
-  height: 80px;
-}
-#banner{
-  border-radius: 50px;
+.font-medium{
+    font-size: 1.1rem;
 }
 </style>
