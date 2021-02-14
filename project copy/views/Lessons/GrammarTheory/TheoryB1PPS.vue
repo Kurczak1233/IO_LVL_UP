e9242a17434feadda58ffc4415bdeda143ac4241
Michał Kupczak <kurczak12333@gmail.com>
2021-02-14 12:14:22 +0100

    Add content

--- project copy/views/Lessons/GrammarTheory/TheoryB1PPS.vue
+++ project copy/views/Lessons/GrammarTheory/TheoryB1PPS.vue
@@ -1,5 +1,5 @@
 <template>
-<b-container class="container text-center">
+<b-container class="container text-center mb-3">
   <b-row class="">
     <b-col class="mb-2" cols="12"> <img id="banner" class="img-fluid" src="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/src/assets/Baner_JS.png?raw=true" alt="Logo listening"></b-col>
   </b-row>
@@ -9,7 +9,48 @@
   </b-row>
 </b-container>
 <b-container class="col-12 mr-auto ml-auto background-bluish p-3" fluid>
-      <b-col class="text-left margin4"><p>Present perfect simple:<p><p> consectetur adipiscing elit. Duis eu nulla at neque rhoncus eleifend. Aenean ac arcu sem? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit reiciendis amet optio earum commodi minus laudantium vitae id voluptate at facere, sed nobis eligendi et quisquam odit repellat dolor vero. Est ipsum eum corporis cumque quod praesentium, ad at consectetur similique tempora veniam modi facilis sed officiis, doloremque illo quaerat ex! Ipsa, facere in! Sit recusandae ipsa nemo libero eveniet, iste doloremque repudiandae quidem in, tempore facilis veritatis sint mollitia, quasi culpa alias quos pariatur? Maxime, aliquam mollitia quo cupiditate nostrum sed illum porro totam voluptates a doloremque voluptatem velit iure sunt ipsum ratione! Est quod saepe atque unde velit harum, commodi necessitatibus animi veniam aut illo amet nobis, optio fugit minus? Voluptates autem quasi praesentium quisquam nam distinctio ullam velit pariatur iusto ab harum quas obcaecati eos temporibus numquam voluptatibus facere, modi vel reprehenderit quidem dolorem odit consectetur! Cupiditate aut ut enim! Reprehenderit atque tenetur fuga, aspernatur suscipit blanditiis dolores, ipsam magnam officiis vero similique molestias voluptas cumque facilis? Qui tenetur harum fugiat ipsa corporis reiciendis totam quam velit dolor odit, fuga temporibus consectetur et ipsum necessitatibus at! Assumenda, laudantium molestiae ipsa illo omnis tenetur minima atque a voluptas nesciunt vitae dolores enim commodi sint, impedit voluptatum perferendis sequi?</p>
+      <b-col class="text-left margin4"><p>Present perfect simple:<p><p>Unfinished Actions
+1: We use this tense when we want to talk about unfinished actions or states or habits that started in the past and continue to the present. Usually we use it to say 'how long' and we need 'since' or 'for'. We often use stative verbs.
+I've known Karen since 1994.
+She's lived in London for three years.
+I've worked here for six months.
+'Since' and 'For'
+We use 'since' with a fixed time in the past (2004, April 23rd, last year). The fixed time can be another action, which is in the past simple (since I was at school, since I arrived).
+I've known Sam since 1992.
+I've liked chocolate since I was a child.
+She's been here since 2pm.
+We use 'for' with a period of time (2 hours, three years, six months).
+I've known Julie for ten years.
+I've been hungry for hours.
+She's had a cold for a week.
+Finished Actions
+2: Life experience. These are actions or events that happened sometime during a person's life. We don't say when the experience happened, and the person needs to be alive now. We often use the words 'ever' and 'never' here.
+I have been to Tokyo.
+They have visited Paris three times.
+We have never seen that film.
+3: With an unfinished time word (this month, this week, today). The period of time is still continuing.
+I haven't seen her this month.
+She's drunk three cups of coffee today.
+I've already moved house twice this year!
+We CAN'T use the present perfect with a finished time word.
+NOT:I've seen him yesterday.
+4: A finished action with a result in the present (focus on result). We often use the present perfect to talk about something that happened in the recent past, but that is still true or important now. Sometimes we can use the past simple here, especially in US English.
+I've lost my keys (so I can't get into my house).
+She's hurt her leg (so she can't play tennis today).
+They've missed the bus (so they will be late).
+5: We can also use the present perfect to talk about something that happened recently, even if there isn't a clear result in the present. This is common when we want to introduce news and we often use the words 'just / yet / already / recently'. However, the past simple is also correct in these cases, especially in US English.
+The Queen has given a speech.
+I've just seen Lucy.
+The Mayor has announced a new plan for the railways.
+Been and Gone
+In this tense, we use both 'been' and 'gone' as the past participle of 'go', but in slightly different circumstances. We use 'been' (often when we talk about life experience) to mean that the person we're talking about visited the place and came back.
+I've been to Paris (in my life, but now I'm in London, where I live).
+She has been to school today (but now she's back at home).
+They have never been to California.
+We use 'gone' (often when we are talking about an action with a result in the present) to mean that the person went to the place and is at the place now.
+Where's John? He's gone to the shops (he's at the shops now).
+Julie has gone to Mexico (now she's in Mexico).
+They've gone to Japan for three weeks (now they're in Japan).</p>
         <p class="text-md-left text-center"><router-link to="/Grammar">Return to grammar</router-link></p>
       </b-col>
     </b-container>
