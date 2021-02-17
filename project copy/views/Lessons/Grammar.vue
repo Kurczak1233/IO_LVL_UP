0cb06e3a32dd3c1cd4761ff98c867a03335c3206
Kurczak1233 <kurczak12333@gmail.com>
2021-02-17 17:43:41 +0100

    change grammar

--- project copy/views/Lessons/Grammar.vue
+++ project copy/views/Lessons/Grammar.vue
@@ -3,7 +3,7 @@
    <UserNavigation></UserNavigation>
   <b-container class="container text-center p-3 mb-3 mt-3 mr-auto ml-auto margin5 background-bluish" fluid>   
       <b-row class="p-4">
-        <b-col class="mb-3 text-size-big mt-auto mb-auto"  cols="12"> Grammar lessons available on <span class="text-danger">{{level}}</span> level </b-col>
+        <b-col class="mb-3 text-size-big mt-auto mb-auto"  cols="12"> Grammar lessons available on  <span class="text-danger">{{level}}</span> level </b-col>
       </b-row>
   </b-container>
 
