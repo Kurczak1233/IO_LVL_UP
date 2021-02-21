1fdfdee5e6f4f3c53ba1d5d8cdaa24669096a222
Kurczak1233 <kurczak12333@gmail.com>
2021-02-21 14:32:28 +0100

    fix bug

--- project copy/views/Lessons/File.vue
+++ project copy/views/Lessons/File.vue
@@ -1,5 +1,6 @@
 <template>
-<b-container class="col-45 col-md-35 col-xl-10 mr-auto ml-auto margin5 background-bluish" fluid>
+<body>
+<b-container class="container  p-3 mb-3 mt-1 mr-auto ml-auto margin5 background-bluish" fluid>
 <b>Download</b>
   
 <table class="table table-striped">
@@ -37,8 +38,15 @@
       <td><a href="https://github.com/Kurczak1233/IO_LVL_UP/blob/Daniel/project%20copy/views/Lessons/download/c2.docx?raw=true">Download C2</a></td>
     </tr>
       </tbody>
+    
 </table>
+    <b-row>
+  <b-col class="text-center mt-3">
+      <p class="text-size-big"><router-link to="/Grammar" class="text-primary">Return to grammar</router-link></p>
+  </b-col>
+</b-row>
     </b-container>  
+</body>
 </template>
 
 <script>
@@ -48,12 +56,5 @@ name: 'File',
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-l {text-align: left}
-c {color: black}
-c:active {background-color: green}
-f {color: black}
-f:active {background-color: red}
-#banner{
-  border-radius: 50px;
-}
+
 </style>
